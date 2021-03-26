04194a0305af3fe4a046121089c151e54ef8962f
Adam Mohammed <13.adamm@gmail.com>
2021-03-26 15:23:19 -0400

    updated ruby examples

---
+++ ex34.rb
@@ -0,0 +1,9 @@
+# Exercise 34: Accessing Elements of Arrays
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is #{animals[0]}"
+
+puts "The first animal is #{animals[0]}"
+
+puts "The third animal is #{animals[2]}"
